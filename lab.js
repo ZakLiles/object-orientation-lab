0bf5e9e085b34a820e7dcaded307b6eefe520265
Zak Liles <zak.liles@gmail.com>
2021-11-26 14:58:09 -0700

    Problem 7 Completed

--- lab.js
+++ lab.js
@@ -90,8 +90,7 @@ var carDetails = {
 */
 
 //Code Here
-
-
+let {color, make, model, year} = carDetails;
 
 //////////////////////////// PROBLEM 7 ////////////////////////////
 
@@ -103,7 +102,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  let {firstName, lastName, title} = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
